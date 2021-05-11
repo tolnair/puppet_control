b45860888aba3d58b195df9e5c431472feb1f53f
Tolnai Richárd <66158707+tolnair@users.noreply.github.com>
2021-05-11 10:26:48 +0200

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,18 @@
 node default {
+  file { '/root/README':
+    ensure  => file,
+    contetn => 'This is a text!',
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/ {
+  include role::app_server
+}
+
+node /^db/ {
+  include role::db_server
 }
